da4e8f80301abfbf76ed63751ca273cd6d47b0a7
niawjunior <niawkung@gmail.com>
2023-06-13 16:53:35 +0700

    update routes structure

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
+const User = require("../models/User");
 
 const BlacklistedToken = require("../models/BlacklistedToken");
 
